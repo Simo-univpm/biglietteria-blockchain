d61d47b2635d52e376249e635d6a58d0a4d11b10
Giacomo.pierig <51120731+Giacomo-pierig@users.noreply.github.com>
2021-12-07 15:00:37 +0100

    Delete system directory

--- system/middlewares/index.js
+++
@@ -1,9 +0,0 @@
-const verifyToken = require('./verifyToken');
-const checkLogin = require('./checkLogin');
-const checkPrivileges = require('./checkPrivileges');
-
-module.exports = {
-    verifyToken: verifyToken,
-    checkLogin: checkLogin,
-    checkPrivileges: checkPrivileges
-}
--- system/model/index.js
+++
@@ -1,9 +0,0 @@
-const User = require('./user');
-const Event = require('./event');
-const Ticket = require('./ticket');
-
-module.exports = {
-    User: User,
-    Event: Event,
-    Ticket: Ticket
-}
