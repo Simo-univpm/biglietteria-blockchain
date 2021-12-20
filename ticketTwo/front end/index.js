886d4d783829a84f28d514f683e934e09eb5ac44
Giacomo.pierig <51120731+Giacomo-pierig@users.noreply.github.com>
2021-12-20 14:53:22 +0100

    Add files via upload

--- ticketTwo/front end/index.js
+++ ticketTwo/front end/index.js
@@ -1,11 +1,11 @@
 const express = require("express")
 
 module.exports = {
-    Annullatore: require("./web pages/annullatore"),
-    Staff_biglietteria: require("./web pages/biglietteria"),
-    Cliente: require("./web pages/cliente"),
+    Annullatore: require("./web pages/Modulo annullatore biglietti"),
+    Staff_biglietteria: require("./web pages/Modulo gestione biglietti"),
+    Cliente: require("./web pages/Modulo vendite biglietti"),
     CSS: express.static(__dirname+"/CSS"),
-    Organizzatore_eventi: require("./web pages/event manager"),
+    Organizzatore_eventi: require("./web pages/Modulo amministrazione evento"),
     Immagini: express.static(__dirname+"/images"),
     Scripts: express.static(__dirname+"/scripts"),
     UtenteOspite: require("./web pages/utente ospite")
